39fa8406a123b69743072cdd8449cef6f2962ec3
Mark Rea <markrea366@gmail.com>
2020-05-01 15:03:09 +0100

    installed nock and used to intercept http requests

--- src/app.js
+++ src/app.js
@@ -9,8 +9,11 @@ const {
 const app = express();
 
 app.get('/', mainController);
+
 app.get('/jokes', jokesController);
+
 app.get('/joke/random', randomController);
+
 app.get('/joke/random/personal/:first/:last', personalController);
 
 module.exports = app;
--- src/controllers.js
+++ src/controllers.js
@@ -1,19 +1,27 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const request = require('request');
+
 const mainController = (req, res) =>
   res.send({
     message: 'Welcome to my jokes API!',
   });
 
 const jokesController = (req, res) =>
-  res.send({
-    message: 'This is the all jokes endpoint',
+  request('https://api.icndb.com/jokes', (error, jokesApiResponse) => {
+    if (error) {
+      console.log(error);
+    }
+    const parsedResponse = JSON.parse(jokesApiResponse.body);
+
+    res.send({ jokes: parsedResponse.value });
   });
 
-const randomController = (req,res) =>
+const randomController = (req, res) =>
   res.send({
     message: 'This is the random joke endpoint',
   });
 
-const personalController = (req, res) => 
+const personalController = (req, res) =>
   res.send({
     message: 'This is the personal joke endpoint',
   });
